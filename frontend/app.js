0ce52e16327b0c39ff65957f4136389da917dca3
kbugga230209-png <kbugga230209@gmail.com>
2026-03-15 01:17:12 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,17 +1,58 @@
-const button = document.querySelector("button")
-const result = document.querySelector(".result")
+function generateOutfit(){
 
-button.addEventListener("click", function(){
+const height = document.getElementById("height").value
+const weight = document.getElementById("weight").value
+const style = document.getElementById("style").value
+const budget = document.getElementById("budget").value
 
-result.innerHTML = `
-<h2>Recommended Outfit</h2>
+const resultBox = document.getElementById("result")
+const outfitText = document.getElementById("outfitText")
 
-<p>
-Oversized Hoodie<br>
-Black Cargo Pants<br>
-White Sneakers
-</p>
+let outfit = ""
+
+if(style === "Streetwear"){
+
+outfit = `
+Oversized Hoodie  
+Black Cargo Pants  
+Nike Air Force Sneakers
 `
 
-})
+}
 
+else if(style === "Minimal"){
+
+outfit = `
+White T-Shirt  
+Slim Fit Jeans  
+White Sneakers
+`
+
+}
+
+else if(style === "Formal"){
+
+outfit = `
+Tailored Blazer  
+Dress Shirt  
+Slim Trousers  
+Oxford Shoes
+`
+
+}
+
+else if(style === "Gym"){
+
+outfit = `
+Compression T-Shirt  
+Athletic Shorts  
+Running Shoes
+`
+
+}
+
+outfitText.innerText = outfit
+
+resultBox.classList.add("show")
+
+}
