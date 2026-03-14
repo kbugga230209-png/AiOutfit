a71acd5bd31827094ff5aff8162763b958b0a42a
kbugga230209-png <kbugga230209@gmail.com>
2026-03-15 01:12:11 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,17 @@
+const button = document.querySelector("button")
+const result = document.querySelector(".result")
+
+button.addEventListener("click", function(){
+
+result.innerHTML = `
+<h2>Recommended Outfit</h2>
+
+<p>
+Oversized Hoodie<br>
+Black Cargo Pants<br>
+White Sneakers
+</p>
+`
+
+})
 
